6fce9a363b994dbe6082d22b686a942bfbd452f7
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-07-01 11:11:29 -0400

    completed

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,9 @@ import React from "react"
 const Article = ({blogTitle="January 1, 1970", blogDate, blogPreview}) => {
   return (
       <article>
-            <h3>{blogTitle}</h3>
-            <small>{blogDate}</small>
-            <p>{blogPreview}</p>
+            <h3 key={blogTitle}> {blogTitle}</h3>
+            <small key={blogDate}>{blogDate}</small>
+            <p key={blogPreview}>{blogPreview}</p>
       </article>
   )
 }
